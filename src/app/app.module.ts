4aa90cb38a2a0813744585037a496e88f273c5b3
Dominik_Gadzina <epros98@gmail.com>
2023-01-02 18:02:52 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CountryAboutComponent } from './country-about/country-about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryAboutComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/country-about/country-about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-about',
+  templateUrl: './country-about.component.html',
+  styleUrls: ['./country-about.component.css']
+})
+export class CountryAboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
